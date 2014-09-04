4af98326806c43df68abb23586da2f636dc55eaf
mtrop <mtropiano@gmail.com>
2014-09-04 02:21:02 -0400

    Added preliminary objects.

---
+++ src/com/blackrook/archetext/ArcheTextObject.java
@@ -0,0 +1,157 @@
+package com.blackrook.archetext;
+
+import com.blackrook.commons.Common;
+import com.blackrook.commons.linkedlist.Stack;
+
+/**
+ * The object representation for all ArcheText objects and values.
+ * ArcheText objects have types and can inherit values from one or more objects.
+ * @author Matthew Tropiano
+ */
+public class ArcheTextObject
+{
+	/**
+	 * ArcheText object internal types.
+	 */
+	public static enum Type
+	{
+		/** Boolean type. */
+		BOOLEAN,
+		/** Numeric type. */
+		NUMBER,
+		/** String type. */
+		STRING,
+		/** Array type. Stored as ArcheTextObject[]. */
+		ARRAY,
+		/** Set type. Stored as Hash<ArcheTextObject>. */
+		SET,
+		/** Object type for objects, or null. Stored as HashMap<String, ArcheTextObject>, or null. */
+		OBJECT;
+	}
+
+	/** Object type. */
+	private String type;
+	/** Object name. */
+	private String name;
+	
+	/** Object hierarchy parents. */
+	private Stack<ArcheTextObject> parents;
+
+	/** Object value type. */
+	private Type valueType;
+	/** Object internal value. */
+	private Object value;
+
+	/**
+	 * Creates a new anonymous ArcheTextObject.
+	 */
+	public ArcheTextObject()
+	{
+		this(null, null);
+	}
+
+	/**
+	 * Creates a new default ArcheTextObject with a type.
+	 */
+	public ArcheTextObject(String type)
+	{
+		this(type, null);
+	}
+
+	/**
+	 * Creates a new ArcheTextObject with a type and name.
+	 */
+	public ArcheTextObject(String type, String name)
+	{
+		if (!Common.isEmpty(name) && Common.isEmpty(type))
+			throw new IllegalArgumentException("type cannot be empty if the name is.");
+		this.type = type;
+		this.name = name;
+	}
+	
+	/**
+	 * Returns the type name of this object.
+	 */
+	public String getType()
+	{
+		return type;
+	}
+
+	/**
+	 * Returns the declarative name of this object.
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * Returns the object's parents, in order of precedence.
+	 */
+	public Iterable<ArcheTextObject> getParents()
+	{
+		return parents;
+	}
+
+	/**
+	 * Returns the value type.
+	 */
+	public Type getValueType()
+	{
+		return valueType;
+	}
+
+	/**
+	 * Returns the value.
+	 */
+	public Object getValue()
+	{
+		return value;
+	}
+
+	/**
+	 * Returns true if this object has no type nor name.
+	 */
+	public boolean isAnonymous()
+	{
+		return type == null && name == null;
+	}
+
+	/**
+	 * Returns true if this object has a type but no name.
+	 */
+	public boolean isDefault()
+	{
+		return type != null && name == null;
+	}
+
+	/**
+	 * Pushes a parent onto the parent stack.
+	 * @param parent the parent to push.
+	 */
+	public void pushParent(ArcheTextObject parent)
+	{
+		if (parents == null)
+			parents = new Stack<ArcheTextObject>();
+		parents.push(parent);
+	}
+
+	/**
+	 * Removes a parent from the parent stack.
+	 * @param parent the parent to remove.
+	 * @return true if removed.
+	 */
+	public boolean removeParent(ArcheTextObject parent)
+	{
+		if (parents == null)
+			return false;
+		boolean out = parents.remove(parent);
+		if (parents.isEmpty())
+			parents = null;
+		return out;
+	}
+	
+	
+	// TODO: Add set value and get value (and get-value-as).
+	
+}
---
+++ src/com/blackrook/archetext/ArcheTextRoot.java
@@ -0,0 +1,178 @@
+package com.blackrook.archetext;
+
+import com.blackrook.commons.Common;
+import com.blackrook.commons.hash.HashMap;
+
+/**
+ * The root of an ArcheText Hierarchy.
+ * This is an object that contains other objects.
+ * @author Matthew Tropiano
+ */
+public class ArcheTextRoot
+{
+	/**
+	 * An object's set of descendants - default and named. 
+	 */
+	private static class DescendantSet
+	{
+		/** Default object (no name). */
+		private ArcheTextObject defaultObject;
+		/** Name to object set. */
+		private HashMap<String, ArcheTextObject> nameSet;
+		
+		boolean isEmpty()
+		{
+			return !containsDefault() && Common.isEmpty(nameSet);
+		}
+		
+		/** Returns true if this contains a default object. */
+		boolean containsDefault()
+		{
+			return defaultObject != null;
+		}
+		
+		/** Returns true if this contains a default object. Empty name is default. */
+		boolean contains(String name)
+		{
+			if (Common.isEmpty(name))
+				return containsDefault();
+			else if (nameSet == null)
+				return false;
+			else
+				return nameSet.containsKey(name);
+		}
+		
+		/** Adds an object. Empty name is default. */
+		void add(String name, ArcheTextObject object)
+		{
+			if (Common.isEmpty(name))
+				defaultObject = object;
+			else
+			{
+				if (nameSet == null)
+					nameSet = new HashMap<String, ArcheTextObject>();
+				nameSet.put(name, object);
+			}
+		}
+		
+		/** Removes an object and returns this reference. Empty name is default.*/
+		ArcheTextObject remove(String name)
+		{
+			if (Common.isEmpty(name))
+			{
+				ArcheTextObject out = defaultObject;
+				defaultObject = null;
+				return out;
+			}
+			else if (nameSet == null)
+				return null;
+			else
+				return nameSet.removeUsingKey(name);
+		}
+		
+		/** Removes an object and returns this reference. Empty name is default.*/
+		ArcheTextObject get(String name)
+		{
+			if (Common.isEmpty(name))
+				return defaultObject;
+			else if (nameSet == null)
+				return null;
+			else
+				return nameSet.removeUsingKey(name);
+		}
+		
+	}
+
+	/** Object descendants by type. */
+	private HashMap<String, DescendantSet> descendants;
+
+	/** 
+	 * Creates a new root. 
+	 */
+	public ArcheTextRoot()
+	{
+		descendants = null;
+	}
+	
+	/**
+	 * Gets a default object by type.
+	 * @param type object type.
+	 * @return the corresponding object, or null if no default.
+	 */
+	public ArcheTextObject get(String type)
+	{
+		if (descendants == null)
+			return null;
+		
+		DescendantSet set = descendants.get(type);
+		if (set == null)
+			return null;
+		
+		return set.get(null);
+	}
+
+	/**
+	 * Gets an object by type and name. Empty name is default.
+	 * @param type object type.
+	 * @param name object name.
+	 * @return the corresponding object, or null if not found.
+	 */
+	public ArcheTextObject get(String type, String name)
+	{
+		if (descendants == null)
+			return null;
+		
+		DescendantSet set = descendants.get(type);
+		if (set == null)
+			return null;
+		
+		return set.get(name);
+	}
+	
+	/**
+	 * Adds an object. Empty name is default.
+	 */
+	public void add(ArcheTextObject object)
+	{
+		if (descendants == null)
+			descendants = new HashMap<String, ArcheTextRoot.DescendantSet>();
+		
+		String type = object.getType();
+		DescendantSet set = descendants.get(type);
+		if (set == null)
+			descendants.put(type, set = new DescendantSet());
+		
+		set.add(object.getName(), object);
+	}
+	
+	/**
+	 * Removes an object.
+	 */
+	public boolean remove(ArcheTextObject object)
+	{
+		if (descendants == null)
+			return false;
+		
+		String type = object.getType();
+		DescendantSet set = descendants.get(type);
+		if (set == null)
+			return false;
+		
+		String name = object.getName();
+		if (set.contains(name))
+		{
+			set.remove(name);
+			if (set.isEmpty())
+				descendants.removeUsingKey(type);
+			return true;
+		}
+		
+		if (descendants.isEmpty())
+			descendants = null;
+		
+		return false;
+	}
+	
+	
+	
+}
